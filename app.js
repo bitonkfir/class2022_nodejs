554321714cd61d753b1c0949b34ca925ee32f9c8
bitonkfir <kfirbiton2@ymail.com>
2022-04-23 17:28:35 +0300

    test 2

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const app = express()
 const dotenv = require('dotenv').config()
 const port = process.env.port
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => {
+    console.error(error)
+})
+db.once('open', () => {
+    console.log('coneceted to mongodb')
+})
+
+const body_parser = require('body-parser')
+
+app.use(body_parser.urlencoded({ extended: true, limit: '1mb' }))
+app.use(body_parser.json())
+
 
 const routes = require('./routes/post_routes')
 app.use('/post', routes)
+
 
 app.listen(port, () => {
     console.log('server started on port' + port);
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const post = require('../controllers/post')
+
+
 router.get('/', post.getALLPosts)
 
 router.post('/', post.createNewPost)
+
+router.get('/:id', post.getPostById)
+
+
+
 module.exports = router
